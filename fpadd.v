88ef4d8785bc43afa4631da212b55f8bd7ea2af2
AyushJam <ayushjamdar@gmail.com>
2022-09-13 20:27:14 +0530

    Corrected major mistakes and made the leading one finder combinational. Added a few display statements here and there. Result: All test cases passed.

--- fpadd.v
+++ fpadd.v
@@ -58,7 +58,8 @@ reg [2:0] NextState;
 	reg sign_r;
 	reg [7:0] exp_r;
 	reg [25:0] mant_r;
-	reg b25, b24;
+	reg b23, b24;
+	reg [4:0] one_index;
 	
 
 // Synchronous State Transition
@@ -72,6 +73,7 @@ end
 // Conditional State Transition
 always @(*) begin 
 	NextState = CurrentState;
+	// default
 	
 	case (CurrentState)
 		STATE_Initial: begin
@@ -88,7 +90,8 @@ always @(*) begin
 			end
 			else if (start) begin
 				// 1. Start parsing input data
-				$display ("a = %b", a);
+				//$display ("a = %d, b = %d\n", a, b);
+				$display ("a = %b, b = %b", a, b);
 				done 	= 	0;
 				sum  	= 	32'b0;
 				sign_a 	= 	a[31];
@@ -97,6 +100,7 @@ always @(*) begin
 				exp_b 	= 	b[30:23];
 				mant_a 	= 	{2'b00, a[22:0]};   
 				mant_b 	= 	{2'b00, b[22:0]};
+				
 				// just the mantissa read for now, will update later
 				
 				// 2. Handle special cases
@@ -122,6 +126,7 @@ always @(*) begin
 				end
 				else begin
 					// 3. actual non-exceptional computation
+					$display ("No special case.");
 					NextState = STATE_1;
 				end
 			end
@@ -141,12 +146,15 @@ always @(*) begin
 			
 			// 3.2 compare exponents
 			if (exp_a > exp_b) begin
+				$display ("A had a bigger exponent.");
 				NextState = STATE_2;	
 			end
 			else if (exp_a < exp_b) begin
+				$display ("B had a bigger exponent.n");
 				NextState = STATE_3;
 			end
 			else begin // they are equal
+				$display ("Equal exponents.");
 				exp_r = exp_a;
 				NextState = STATE_4;
 			end
@@ -165,16 +173,19 @@ always @(*) begin
 		end
 		STATE_4: begin
 			// 3.3 Compute Addition
-			mant_r = mant_a + mant_b;
+			mant_r = {mant_a[24] ,mant_a} + {mant_b[24] ,mant_b};
+			$display("mant_a = %b, mant_b = %b", mant_a , mant_b);
 			// 3.4 Sign of the result
 			if (mant_r[25]) begin
 				// MSB indicates sign
 				// negative
+				$display("result negative.");
 				sign_r = 1;
 				mant_r = -mant_r;
 			end
 			else begin
 				// postitive
+				$display("result positive.");
 				sign_r = 0;
 			end
 			NextState = STATE_5;
@@ -182,42 +193,74 @@ always @(*) begin
 		STATE_5: begin
 			// 3.5 Normalize
 			// mant_r is now unsigned
-			b25 = mant_r[25];
+			b23 = mant_r[23];
 			b24 = mant_r[24];
-			
 			if (!mant_r) begin
 				// 3.5.1 numbers cancelled out
 				exp_r = 0;
 				NextState = STATE_6;
 			end
-			else if ((!b25) && (b24)) begin
+			else if ((!b24) && (b23)) begin
 				// 3.5.2 already normalized 
 				// no need to normalize
+				$display("no need to normalize");
 				NextState = STATE_6;
 			end
-			else if (b25) begin
+			else if (b24) begin
 				// 3.5.3 Overflow - renormalize
+				$display("normalize for overflow\n");
 				mant_r = {1'b0, mant_r[25:1]};
 				exp_r  = exp_r + 1'b1;
 				NextState = STATE_6;
 			end
 			else begin 
 				// 3.5.4 Search for leading one
-				if(b24) begin
+				if(b23) begin
 					// normalized
+					$display("normalized for small delta\n");
 					NextState = STATE_6;
 				end
 				else begin 
 					// keep looping until normalized
-					mant_r	= {mant_r[24:0], 1'b0}; 
-					exp_r	= exp_r - 1'b1;
-					b24 	= mant_r[24];
-					NextState = STATE_5;
+				
+					$display("normalize for small delta.");
+					case (1)
+						mant_r[22]: begin one_index = 22; end
+						mant_r[21]: begin one_index = 21; end
+						mant_r[20]: begin one_index = 20; end
+						mant_r[19]: begin one_index = 19; end
+						mant_r[18]: begin one_index = 18; end
+						mant_r[17]: begin one_index = 17; end
+						mant_r[16]: begin one_index = 16; end
+						mant_r[15]: begin one_index = 15; end
+						mant_r[14]: begin one_index = 14; end
+						mant_r[13]: begin one_index = 13; end
+						mant_r[12]: begin one_index = 12; end
+						mant_r[11]: begin one_index = 11; end
+						mant_r[10]: begin one_index = 10; end
+						mant_r[9]: begin one_index = 9; end
+						mant_r[8]: begin one_index = 8; end
+						mant_r[7]: begin one_index = 7; end
+						mant_r[6]: begin one_index = 6; end
+						mant_r[5]: begin one_index = 5; end
+						mant_r[4]: begin one_index = 4; end
+						mant_r[3]: begin one_index = 3; end
+						mant_r[2]: begin one_index = 2; end
+						mant_r[1]: begin one_index = 1; end
+						mant_r[0]: begin one_index = 0; end
+
+					endcase
+					$display("one_index = %b", one_index);
+					mant_r = mant_r << (23 - one_index);
+					exp_r  =  exp_r - (23 - one_index);
+					NextState = STATE_6;
+				
 				end
 			end
 		end
 		STATE_6: begin
 			// 4. Generate output
+			$display("sign = %b, exp = %b, mant = %b\n", sign_r, exp_r, mant_r);
 			sum	= {sign_r, exp_r, mant_r[22:0]};
 			done	= 1'b1;
 			NextState = STATE_Initial;
